a1a302ff9b2ec8c7456e792c05e26b456a86c67c
Denis <oterohina@gmail.com>
2022-11-05 21:22:09 +0400

    Хук useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,60 @@
-import React, { useState } from "react";
+import React, { useLayoutEffect, useReducer, useRef, useState } from "react";
 import "./App.css";
+import { COMPLETE, initialState, reducer, UNCOMPLETE } from "./reducer";
+import { Dispatch } from "redux";
 
 interface IData {
   name: string;
   phone: number;
 }
 
+interface ITodo {
+  name: string;
+  isCompleted: boolean;
+}
+
 function App() {
   const [data, setData] = useState<IData>({
     name: "",
     phone: 0,
-  } as IData);
+  });
 
   const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setData({ ...data, name: e.target.value });
   };
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useLayoutEffect(() => {
+    console.log(data.phone);
+    console.log(inputRef.current?.value);
+  });
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div className="App">
-      <input type="text" value={data.name} onChange={handleInput} />
+      <input
+        type="text"
+        value={data.name}
+        onChange={handleInput}
+        ref={inputRef}
+      />
       <h2>{data.name}</h2>
+      {state.map((i: ITodo, id: number) => (
+        <div
+          key={id}
+          style={{ color: i.isCompleted ? "grey" : "black" }}
+          onClick={() =>
+            dispatch({
+              type: i.isCompleted ? UNCOMPLETE : COMPLETE,
+              payload: i.name,
+            })
+          }
+        >
+          {i.name}
+        </div>
+      ))}
     </div>
   );
 }
---
+++ src/reducer.ts
@@ -0,0 +1,40 @@
+export const COMPLETE = "COMPLETED";
+export const UNCOMPLETE = "UNCOMPLETED";
+
+interface IAction {
+  type: string;
+  payload: string;
+}
+
+interface IState {
+  name: string;
+  isCompleted: boolean;
+}
+
+export const initialState = [
+  {
+    name: "Устроиться на работу",
+    isCompleted: false,
+  },
+  {
+    name: "Приготовить вкусный ужин",
+    isCompleted: false,
+  },
+];
+
+export const reducer = (state: IState[], action: IAction) => {
+  switch (action.type) {
+    case COMPLETE:
+      return state.map((i) => {
+        if (i.name === action.payload) i.isCompleted = true;
+        return i;
+      });
+    case UNCOMPLETE:
+      return state.map((i) => {
+        if (i.name === action.payload) i.isCompleted = false;
+        return i;
+      });
+    default:
+      return state;
+  }
+};
